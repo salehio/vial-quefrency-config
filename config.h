9a6232af859d5125cef833b820edba4bbaa256d6
Hassan Saleh <hasan@saleh.io>
2022-08-08 13:51:46 -0700

    Initial commit

---
+++ config.h
@@ -0,0 +1,27 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+Copyright 2018 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define VIAL_KEYBOARD_UID {0x56, 0x73, 0x36, 0x3D, 0xB9, 0xEC, 0x6A, 0x18}
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+// #define USE_I2C
